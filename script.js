762ee2cf3396db274f5445fd1f3db57cf8f9bc11
Roselene Gabun <roselenegabun@gmail.com>
2022-12-09 17:42:10 -0800

    Just Cat n Facts Finish!

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const cat_btn = document.getElementById('cat_btn');
-// const dog_btn = document.getElementById('dog_btn');
 const cat_result = document.getElementById('cat_result');
 const cat_fact = document.getElementById('cat-fact');
-// const dog_result = document.getElementById('dog_result');
 
 cat_btn.addEventListener('click', getRandomCat);
-// dog_btn.addEventListener('click', getRandomDog);
 
 function getRandomCat() {
 	fetch('https://aws.random.cat/meow')
@@ -19,16 +16,3 @@ function getRandomCat() {
             cat_fact.innerHTML = `"${data.fact}"`
         })
 }
-
-// function getRandomDog() {
-// 	fetch('https://random.dog/woof.json')
-// 		.then(res => res.json())
-// 		.then(data => {
-// 			if(data.url.includes('.mp4')) {
-// 				getRandomDog();
-// 			}
-// 			else {
-// 				dog_result.innerHTML = `<img src=${data.url} alt="dog" />`;
-// 			}
-// 		});
-// }
